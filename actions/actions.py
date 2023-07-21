7c6167a0b0a9e2d1183c9a4632d3f546b3b67e78
Bennour <mohamed8bennour@gmail.com>
2023-07-21 10:00:47 +0100

    Initial commit for dev branch

--- actions/actions.py
+++ actions/actions.py
@@ -1,51 +1,30 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List, Union
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
-from rasa_sdk.forms import FormAction
+from rasa_sdk.events import SlotSet, Form
 
-class AfficherLocalisationsAction(Action):
+class ValidateEmploiForm(Action):
     def name(self) -> Text:
-        return "afficher_localisations"
+        return "emploi_form"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        locations = [
-            "Ariana", "Beja", "Ben Arous", "Bizerte", "Gabes", "Gafsa", "Jendouba", "Kairouan", "Kasserine",
-            "Kebili", "Le Kef", "Mahdia", "Manouba", "Medenine", "Monastir", "Nabeul", "Sfax", "Sidi Bouzid",
-            "Siliana", "Sousse", "Tataouine", "Tozeur", "Tunis", "Zaghouan"
-        ]
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        required_slots = ["slot_location", "slot_type_contrat", "slot_domaine_emploi"]
+        missing_slots = []
 
-        buttons = []
-        for location in locations:
-            payload = f"/choisir_localisation{{'location': '{location}'}}"
-            button = {"title": location, "payload": payload}
-            buttons.append(button)
-        
-        dispatcher.utter_message(
-            text="Dans quelle localisation souhaitez-vous chercher des offres d'emploi ?",
-            buttons=buttons
-        )
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                missing_slots.append(slot_name)
 
-        return []
+        if len(missing_slots) > 0:
+            if "slot_salaire_minimum" in missing_slots or "slot_salaire_maximum" in missing_slots:
+                # Au moins l'un des slots de salaire est manquant
+                if "slot_salaire_minimum" not in missing_slots:
+                    missing_slots.remove("slot_salaire_maximum")
+                elif "slot_salaire_maximum" not in missing_slots:
+                    missing_slots.remove("slot_salaire_minimum")
 
-class ChoisirLocalisationAction(Action):
-    def name(self) -> Text:
-        return "choisir_localisation"
+            # Répéter la question manquante
+            dispatcher.utter_message(template=f"utter_demande_{missing_slots[0]}")
+            return [SlotSet("requested_slot", missing_slots[0])]
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        location = tracker.latest_message.get('payload').get('location')
-        dispatcher.utter_message(text=f"Vous avez choisi la localisation {location}.")
-
-        return [SlotSet("location", location)]
+        return [Form("emploi_form"), SlotSet("requested_slot", None)]
